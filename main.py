e562aa3e33cd4567c5e2ddd1e780f3aa416bdaf9
TheVoidThatConsumes <lyricalluxor@gmail.com>
2022-06-01 14:19:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
 import random
 
-while True:
-    hand_sign = input("What would it be? (rock, paper, scissor) ===> ")
 
-    possible_actions = ["rock", "paper", "scissor"]
+
+while True:
+    hand_sign = input("Pick a Gesture (R, P, S): ")
+    hand_sign = hand_sign.upper()
+
+    possible_actions = ["R", "P", "S"]
     ai_yourmove = random.choice(possible_actions)
-    print(f"\nYou throw a {hand_sign}, your opponent throws a {ai_yourmove}.\n")
+
+    
 
     if hand_sign == ai_yourmove:
         print(f"Both players threw {hand_sign}. It's a tie!")
     
-    elif hand_sign == "rock":
-        if ai_yourmove == "scissor":
+    elif hand_sign == "R":
+        if ai_yourmove == "S":
+            print("Player (You): Rock")
+            print("CPU: Scissor")
             print("Your Rock breaks your opponent's Scissor. YOU WIN!")
         else:
+            print("Player (You): Rock")
+            print("CPU: Paper")
             print("Your opponent's Paper covers your Rock. YOU LOSE!")
+            
+        break
     
-    elif hand_sign == "paper":
-        if ai_yourmove == "rock":
+    elif hand_sign == "P":
+        if ai_yourmove == "R":
+            print("Player (You): Paper")
+            print("CPU: Rock")
             print("Your Paper covers your opponent's Rock. YOU WIN!")
         else:
+            print("Player (You): Paper")
+            print("CPU: Scissor")
             print("Your opponent's Scissor cuts your Paper. YOU LOSE!")
 
-    elif hand_sign == "scissor":
-        if ai_yourmove == "paper":
+        break
+
+    elif hand_sign == "S":
+        if ai_yourmove == "P":
+            print("Player (You): Scissor")
+            print("CPU: Paper")
             print("Your Scissor cuts through your opponent's Paper. YOU WIN!")
         else:
+            print("Player (You): Scissor")
+            print("CPU: Rock")
             print("Your opponent Rock breaks your Scissor. YOU LOSE!")
 
-
-
-    replay = input("Do you want to try again? (y/n): ")
-    if replay.lower() != "y":
         break
+
+       
 
